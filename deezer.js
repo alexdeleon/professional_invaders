bdcd3e866aee8d03a05f34e230cb5d15bec650f6
Alexander De Leon <alexander.leon@buongiorno.com>
2013-04-24 18:47:30 +0200

    Added function to play tract

--- deezer.js
+++ deezer.js
@@ -9,12 +9,22 @@ $.getScript("http://cdn-files.deezer.com/js/min/dz.js", function(data, textStatu
 			width : 650,
 			height : 80,
 			onload : function(){
- 
-				DZ.player.playRadio(203, 20, function(response){
-						console.log("track list", response.tracks);
-				});
+ 					playRadio(203);
 			}
 		}
 	});
    
 });
+
+function playRadio(radio){
+	DZ.player.playRadio(radio, 20, function(response){
+		console.log("track list", response.tracks);
+	});
+}
+
+
+function playTrack(track){
+	DZ.player.playTracks([track], 0, function(response){
+		console.log("track list", response.tracks);
+	});
+}
