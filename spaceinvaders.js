06965135ca1405cb8b43531cb51e0c5992b6577a
Alessandro Bahgat <ale.bahgat@gmail.com>
2013-04-25 15:32:40 +0200

    Player loses if hit by an enemy

--- spaceinvaders.js
+++ spaceinvaders.js
@@ -818,6 +818,11 @@ var Game = function(canvas, config) {
           --numInvaders;
         }
       }
+
+      if (canon && canon.isHit(invader)) {
+        canon.explode(particles);
+        canon = null;
+      }
       return keepIt;
     });
 
