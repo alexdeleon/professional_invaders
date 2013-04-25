0b48adff68f1e83eae812688e370bd8574848ca2
Alessandro Bahgat <ale.bahgat@gmail.com>
2013-04-25 12:36:48 +0200

    Built braintree backend to accept coin payments

--- app/main.py
+++ app/main.py
@@ -18,6 +18,17 @@ import webapp2
 
 from google.appengine.api import memcache
 
+import braintree
+
+braintree.Configuration.configure(
+    braintree.Environment.Sandbox,
+    "tnxsfx6t24txq93n",
+    "z8ts5ffprj6q62s8",
+    "3ca2081b05252a10e17b1fd95867b41f"
+)
+
+braintree.Configuration.use_unsafe_ssl = True
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
     	self.response.headers['Access-Control-Allow-Origin'] = '*'
@@ -34,7 +45,47 @@ class MainHandler(webapp2.RequestHandler):
     	else:
     		memcache.replace('boss', True)
 
+class Payment(webapp2.RequestHandler):
+    def get(self):
+        self.do()
+    def post(self):
+        self.do()
+    def do(self):
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        amount = self.request.get('amount')
+        number = self.request.get('number')
+        expiration_date = self.request.get('expiration_date')
+
+        params = {
+            "amount": amount,
+            "credit_card": {
+                "number": number,
+                "expiration_date": expiration_date
+            }
+        }
+
+        result = braintree.Transaction.sale(params)
+
+        if result.is_success:
+            print "success!: " + result.transaction.id
+            self.response.write(result.transaction.id);
+            return
+        elif result.transaction:
+            print "Error processing transaction:"
+            print "  message: " + result.message
+            print "  code:    " + result.transaction.processor_response_code
+            print "  text:    " + result.transaction.processor_response_text
+        else:
+            print "message: " + result.message
+            for error in result.errors.deep_errors:
+                print "attribute: " + error.attribute
+                print "  code: " + error.code
+                print "  message: " + error.message
+
+        self.response.write('ERROR:\n' + result.message);
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/pay', Payment)
 ], debug=True)
