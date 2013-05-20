8f3deae577c1f99a4067d7f2f10c387a713f808c
Alessandro Bahgat <ale.bahgat@gmail.com>
2013-05-20 22:18:46 +0200

    Removed Braintree JS SDK

--- braintree.js
+++
@@ -1,6 +0,0 @@
-$.getScript("https://js.braintreegateway.com/v1/braintree.js", function(data, textStatus, jqxhr) {
-	var braintree = Braintree.create("YourClientSideEncryptionKey");
-    braintree.onSubmitEncryptForm('braintree-payment-form');
-});
-
-
